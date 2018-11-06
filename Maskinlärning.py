1afa8e4ee5d431e9e8b25efbfe6270e9ed528c87
Gr8-m8 <martinn.lindbladd@gmail.com>
2018-11-06 21:16:52 +0100

    +karaktarer

--- Maskinlärning.py
+++ Maskinlärning.py
@@ -2,12 +2,9 @@ import random
 import string
 from sklearn import tree
 
-class EvObj:
-  def __init__(self, widthSet, heightSet, tongueSet, teethSet):
-    self.w = widthSet
-    self.h = heightSet
-    self.to = tongueSet
-    self.th = teethSet
+class EvObject:
+  def __init__(self, hairClr, eyeClr, skinClr, accesory):
+    self.attributes = [hairClr, eyeClr, skinClr, accesory]
 
 class PredictManager:
   def __init__(self):
@@ -15,55 +12,44 @@ class PredictManager:
     self.lables = []
 	
   def AddFeature(self, evObject, lableSet):
-    self.features += [[evObject.w, evObject.h, evObject.to, evObject.th]]
+    self.features += [evObject.attributes]
     self.lables.append(lableSet)
 
   def Predict(self, evObject):
     self.clf = tree.DecisionTreeClassifier().fit(self.features, self.lables)
-    return self.clf.predict([[evObject.w, evObject.h, evObject.to, evObject.th]])
+    return self.clf.predict([evObject.attributes])
+
 
 pm = PredictManager()
-pm.AddFeature(EvObj(6, 6, 0, 0), "Ä")
-pm.AddFeature(EvObj(6, 3, 0, 0), "E")
-pm.AddFeature(EvObj(6, 2, 0, 0), "I")
+#pm.AddFeature(EvObject(0, 1, 23, 5), "THOMAS")
+pm.AddFeature(EvObject(1, 1, 1, 11), "THOMAS")
+pm.AddFeature(EvObject(1, 1, 1, 10), "MAX")
+pm.AddFeature(EvObject(1, 1, 1, 8), "Sophie")
 
-pm.AddFeature(EvObj(3, 4, 0, 0), "Ö")
-pm.AddFeature(EvObj(3, 3, 0, 0), "A")
-pm.AddFeature(EvObj(2, 3, 0, 0), "Y")
+pm.AddFeature(EvObject(3, 1, 5, 10), "LUCAS")
+pm.AddFeature(EvObject(7, 1, 5, 10), "Philippe")
+pm.AddFeature(EvObject(3, 6, 5, 7), "JOE")
 
-pm.AddFeature(EvObj(2, 4, 0, 0), "Å")
-pm.AddFeature(EvObj(2, 1, 0, 0), "O")
-
-pm.AddFeature(EvObj(4, 0, 0, 0), "B")
-pm.AddFeature(EvObj(4, 2, 2, 0), "D")
-
-pm.AddFeature(EvObj(4, 1, 1, 1), "F")
-pm.AddFeature(EvObj(3, 3, 0, 0), "G")
-pm.AddFeature(EvObj(3, 3, 0, 0), "H")
-
-pm.AddFeature(EvObj(3, 1, 1, 2), "J")
-pm.AddFeature(EvObj(2, 2, 0, 0), "K")
-pm.AddFeature(EvObj(4, 3, 2, 0), "L")
-pm.AddFeature(EvObj(3, 0, 0, 0), "M")
-pm.AddFeature(EvObj(3, 1, 2, 0), "N")
-
-pm.AddFeature(EvObj(3, 0, 0, 0), "P")
-pm.AddFeature(EvObj(4, 2, 2, 0), "R")
-pm.AddFeature(EvObj(3, 3, 2, 2), "S")
-pm.AddFeature(EvObj(3, 1, 2, 0), "T")
-
-pm.AddFeature(EvObj(3, 0, 0, 1), "V")
-
+#pm.AddFeature(EvObject("Vit", "BRUN", "Blek", "" ), "Paul")
+#pm.AddFeature(EvObject("Vit", "BRUN", "Blek", "" ), "Paul")
+#pm.AddFeature(EvObject("Vit", "BRUN", "Blek", "" ), "Paul")
 
 out = [];
-print("Ord Förutsägare!")
-lng = int(input("Längd på ord: "))
-for i in range(lng):
-  #evObj = EvObj(random.randint(0,3), random.randint(0,3), random.randint(1,3), random.randint(0,2))
-  print("Bokstav: " + str(i + 1))
-  evObj = EvObj(input("Munnens Bred [1 - 6]: "), input("Läppar mellanrum [0 - 6]: "), input("Tungans Position [0 - 2]: "), input("Tändrader [0 - 2]: "))
-  out.append(pm.Predict(evObj))
+print("Vem där!")
+print("Beskriv karaktären du valt för datorn")
+print("Svara med orden: \n Svart, Brun, Gul, \n Blå, Orange, Vit, \n Blek, Inget, Glasögon, \n Hatt, Mustach, SKÄGG")
 
-print("Du sa:")
-for i in out:
-  print(i)
+map = {
+  "SVART": 0.0, "BRUN": 1.0, "ORANGE": 2.0, "GUL": 3.0,  "VIT": 4.0, "BLEK":5.0, "BLÅ":6.0, "INGET": 7.0, "INGEN": 7.0, "GLASÖGON": 8.0, "HATT": 9.0, "SKÄGG": 10.0, "MUSTACH": 11.0
+}
+
+def ask(prompt, map):
+  while True:
+    ans = input(prompt).upper()
+    if ans in map:
+      return map[ans]
+    print("Använd ord från listan!\n")
+
+evObj = EvObject(ask("HårFärg: ",map), ask("Ögonfärg: ",map), ask("Hudfärg: ",map), ask("Accesoar: ",map))
+print("Din karaktär:")
+print(pm.Predict(evObj))
